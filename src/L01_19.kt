35931d602b636e97a15c5673739dd393afb7697b
greegav <greegav@gmail.com>
2018-03-19 13:44:02 +0200

    18-20

---
+++ src/L01_19.kt
@@ -0,0 +1,3 @@
+fun main(args: Array<String>) {
+    print(java.util.Scanner(System.`in`).nextInt()/10)
+}
---
+++ src/L01_20.kt
@@ -0,0 +1,8 @@
+// Создаем Scanner
+val scan = java.util.Scanner(System.`in`)
+fun main(args: Array<String>){
+    //write your code here.
+    var a=scan.nextInt()
+    var b=scan.nextInt()
+    print(5*(b-a))
+}
