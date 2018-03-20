b7d4336d6277cd596f6f51eb311fac0ade66cd05
greegav <greegav@gmail.com>
2018-03-20 15:05:28 +0200

    41

--- src/Lesson1_10/L01_41.kt
+++ src/Lesson1_10/L01_41.kt
@@ -1,29 +1,6 @@
 package Lesson1_10
 
-/*
-Воссоздайте программу.
-Sample Input 1:
-
-55
-Sample Output 1:
-
-true
-Sample Input 2:
-
-74
-Sample Output 2:
-
-false
-Sample Input 3:
-
-80
-Sample Output 3:
-
-true
-Sample Input 4:
-
-51
-Sample Output 4:
-
-false
- */
+fun main(args: Array<String>) {
+    var a=scan.nextInt()
+    print(a%5==0)
+}
